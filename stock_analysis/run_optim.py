d32ceb4a56c36515d77eab5456a75743a66fd362
robinaileen7 <robinaileen7@g.ucla.edu>
2024-02-25 17:28:34 -0800

    Add GBM simulated data and improve the process flow

--- stock_analysis/run_optim.py
+++ stock_analysis/run_optim.py
@@ -2,15 +2,19 @@ import torch.optim as optim
 import torch
 from run_nn import model
 import torch.nn as nn
-from read_img import X_train, y_train, X_test, y_test
 import numpy as np
+import pandas as pd
 
 class run_optim:
-    def __init__(self, alpha):
-        self.optimizer = optim.Adam(model.parameters(), lr=alpha)
-        self.batch_size = round(len(X_train)/2)
+    def __init__(self, alpha, size, X_y_set):
+        self.optimizer = optim.SGD(model.parameters(), lr=alpha, momentum=0.75)
         self.colored = False
-        self.size = 100
+        self.size = size
+        self.X_train = X_y_set[0]
+        self.y_train = X_y_set[1]
+        self.X_test = X_y_set[2]
+        self.y_test = X_y_set[3]
+        self.batch_size = round(len(self.X_train)/2)
 
     @staticmethod
     def loss_function(x):
@@ -22,6 +26,8 @@ class run_optim:
     def model_train(self):
         optimizer = self.optimizer
         batch_size = self.batch_size
+        X_train = self.X_train
+        y_train = self.y_train
 
         for i in range(0, len(X_train)-batch_size):
             optimizer.zero_grad()
@@ -34,6 +40,9 @@ class run_optim:
     def model_test(self):
         run_optim.model_train(self)
         size = self.size
+        X_test = self.X_test
+        y_test = self.y_test
+
         correct = 0
         with torch.no_grad():
             for i in range(0, len(X_test)):
@@ -47,6 +56,9 @@ class run_optim:
         print("The accuracy is")
         print(correct/len(y_test))
 
-for i in np.arange(0.01, 0.017, 0.001):
-    print('tuning parameter: ' + str(i))
-    run_optim(alpha=i).model_test()
+if __name__ == "__main__":
+    from stock_performance import X_train, y_train, X_test, y_test
+    X_y_set = X_train, y_train, X_test, y_test
+    # Use size = 100 for images
+    for i in [0.005, 0.007, 0.008, 0.009, 0.01]:
+        run_optim(alpha=i, size=252, X_y_set=X_y_set).model_test()
--- stock_analysis/stock_performance.py
+++ stock_analysis/stock_performance.py
@@ -3,16 +3,24 @@ import sys
 sys.path.append(os.getcwd())
 from data_prep import df_dict 
 import matplotlib.pyplot as plt
+import numpy as np
+from config import df_len
+import torch
 
-regenerate_pics = True
-threshold = 0
-good = []
-bad = []
+regenerate_pics = False
+threshold = 0.2
+X = torch.Tensor([])
+y = []
 for k in df_dict.keys():
-    if df_dict[k]['Ret_252'].mean() > threshold:
-        good.append(k)
-    else:
-        bad.append(k)
+    _X = torch.Tensor(np.array(df_dict[k])).view(-1, 1, df_len, df_len)
+    X = torch.cat((X, _X), 0)
+    _y = [0, 1] if np.array(df_dict[k])[:, -1].mean() >= threshold else [1, 0]
+    y.append(_y)
+y = torch.Tensor(np.array(y))
+
+train_split = round(len(X) * 0.75)
+X_train, X_test = X[:train_split], X[train_split:]
+y_train, y_test = y[:train_split], y[train_split:]
 
 def save_pics(key_ls, dir_name):
     path = os.getcwd()+'/pics/' + str(dir_name) + '/'
@@ -25,7 +33,14 @@ def save_pics(key_ls, dir_name):
             plt.savefig(path+ str(i) + ".jpg", bbox_inches='tight', pad_inches=0)
         except ValueError as e:
             print(i + ' cannot be converted to an image')
-
-if regenerate_pics:
-    save_pics(key_ls=good, dir_name='good')
-    save_pics(key_ls=bad, dir_name='bad')
+if __name__ == "__main__":
+    if regenerate_pics:
+        good = []
+        bad = []
+        for k in df_dict.keys():
+            if df_dict[k]['Ret_252'].mean() > threshold:
+                good.append(k)
+            else:
+                bad.append(k)
+        save_pics(key_ls=good, dir_name='good')
+        save_pics(key_ls=bad, dir_name='bad')
